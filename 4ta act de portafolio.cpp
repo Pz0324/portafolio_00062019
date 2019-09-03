050bc1b13198b1aca72127b86dacc641d9ba08dc
Pz0324 <00062019@uca.edu.sv>
2019-09-03 16:59:32 -0600

    iniciaizar repositorio

---
+++ 4ta act de portafolio.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct pais_America{
+	
+string nombre;
+string capital;
+float habitantes;
+}
+paisA;
+paisB,
+paisC,
+paisD,
+paisE;
+
+struct pais_Europa{
+	
+string nombre;
+string capital;
+float habitantes;
+
+}
+paisA,
+paisB,
+paisC,
+paisD,
+paisE;
+
+struct pais_Africa{
+	
+string nombre;
+string capital;
+float habitantes;
+
+}
+paisA,
+paisB,
+paisC,
+paisD,
+paisE;
+
+struct pais_Asia{
+	
+string nombre;
+string capital;
+float habitantes;
+
+}
+paisA,
+paisB,
+paisC,
+paisD,
+paisE;	
+	
+struct pais_Oceania{
+	
+string nombre;
+string capital;
+float habitantes;
+
+}
+paisA,
+paisB,
+paisC,
+paisD,
+paisE;	
+
+struct continentes{
+	
+	int main(){
+		
+		
+		
+	}
+
+
